55d5b546f99e6ced60ff7d69460236529b3c0d72
amina1000 <amina1000@yandex.ru>
2021-06-13 00:57:01 +0300

    Исправление ошибок.

--- app/src/main/java/com/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -41,52 +41,52 @@ public class CalculatorActivity extends AppCompatActivity implements Constants {
 
     private void initController() {
         // цифровые кнопки
-        Button button_point = findViewById(R.id.button_point);
-        Button button_0 = findViewById(R.id.button_0);
-        Button button_1 = findViewById(R.id.button_1);
-        Button button_2 = findViewById(R.id.button_2);
-        Button button_3 = findViewById(R.id.button_3);
-        Button button_4 = findViewById(R.id.button_4);
-        Button button_5 = findViewById(R.id.button_5);
-        Button button_6 = findViewById(R.id.button_6);
-        Button button_7 = findViewById(R.id.button_7);
-        Button button_8 = findViewById(R.id.button_8);
-        Button button_9 = findViewById(R.id.button_9);
+        Button buttonPoint = findViewById(R.id.button_point);
+        Button button0 = findViewById(R.id.button_0);
+        Button button1 = findViewById(R.id.button_1);
+        Button button2 = findViewById(R.id.button_2);
+        Button button3 = findViewById(R.id.button_3);
+        Button button4 = findViewById(R.id.button_4);
+        Button button5 = findViewById(R.id.button_5);
+        Button button6 = findViewById(R.id.button_6);
+        Button button7 = findViewById(R.id.button_7);
+        Button button8 = findViewById(R.id.button_8);
+        Button button9 = findViewById(R.id.button_9);
 
         // назначаем единый обработчик для всех цифровых кнопок
-        button_point.setOnClickListener(setNumberOperation);
-        button_0.setOnClickListener(setNumberOperation);
-        button_1.setOnClickListener(setNumberOperation);
-        button_2.setOnClickListener(setNumberOperation);
-        button_3.setOnClickListener(setNumberOperation);
-        button_4.setOnClickListener(setNumberOperation);
-        button_5.setOnClickListener(setNumberOperation);
-        button_6.setOnClickListener(setNumberOperation);
-        button_7.setOnClickListener(setNumberOperation);
-        button_8.setOnClickListener(setNumberOperation);
-        button_9.setOnClickListener(setNumberOperation);
+        buttonPoint.setOnClickListener(setNumberOperation);
+        button0.setOnClickListener(setNumberOperation);
+        button1.setOnClickListener(setNumberOperation);
+        button2.setOnClickListener(setNumberOperation);
+        button3.setOnClickListener(setNumberOperation);
+        button4.setOnClickListener(setNumberOperation);
+        button5.setOnClickListener(setNumberOperation);
+        button6.setOnClickListener(setNumberOperation);
+        button7.setOnClickListener(setNumberOperation);
+        button8.setOnClickListener(setNumberOperation);
+        button9.setOnClickListener(setNumberOperation);
 
         // кнопки очистки
-        Button button_ac = findViewById(R.id.button_ac);
-        Button button_ce = findViewById(R.id.button_ce);
+        Button buttonAc = findViewById(R.id.button_ac);
+        Button buttonCe = findViewById(R.id.button_ce);
         // кнопки команд
-        Button button_percent = findViewById(R.id.button_percent);
-        Button button_divide = findViewById(R.id.button_divide);
-        Button button_multiply = findViewById(R.id.button_multiply);
-        Button button_minus = findViewById(R.id.button_minus);
-        Button button_plus = findViewById(R.id.button_plus);
-        Button button_result = findViewById(R.id.button_result);
+        Button buttonPercent = findViewById(R.id.button_percent);
+        Button buttonDivide = findViewById(R.id.button_divide);
+        Button buttonMultiply = findViewById(R.id.button_multiply);
+        Button buttonMinus = findViewById(R.id.button_minus);
+        Button buttonPlus = findViewById(R.id.button_plus);
+        Button buttonResult = findViewById(R.id.button_result);
 
         // назначаем единый обработчик для кнопок очистки
-        button_ac.setOnClickListener(calcOperation);
-        button_ce.setOnClickListener(calcOperation);
+        buttonAc.setOnClickListener(calcOperation);
+        buttonCe.setOnClickListener(calcOperation);
         // и кнопок команд
-        button_percent.setOnClickListener(calcOperation);
-        button_divide.setOnClickListener(calcOperation);
-        button_multiply.setOnClickListener(calcOperation);
-        button_minus.setOnClickListener(calcOperation);
-        button_plus.setOnClickListener(calcOperation);
-        button_result.setOnClickListener(calcOperation);
+        buttonPercent.setOnClickListener(calcOperation);
+        buttonDivide.setOnClickListener(calcOperation);
+        buttonMultiply.setOnClickListener(calcOperation);
+        buttonMinus.setOnClickListener(calcOperation);
+        buttonPlus.setOnClickListener(calcOperation);
+        buttonResult.setOnClickListener(calcOperation);
     }
 
     public View.OnClickListener setNumberOperation = new View.OnClickListener() {
